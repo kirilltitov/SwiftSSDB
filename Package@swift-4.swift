e209b92299b4f2b64490129d6677da51377e17f8
Kirill Titov <work@kirilltitov.com>
2017-09-14 23:49:09 +0300

    Swift 4 compatible

---
+++ Package@swift-4.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SSDB",
+    products: [
+        .library(name: "SSDB", targets: [ "SSDB" ]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/IBM-Swift/BlueSocket.git", .upToNextMajor(from: "0.12.0"))
+    ],
+    targets: [
+        .target(
+            name: "SSDB",
+            dependencies: [ "Socket" ]
+        ),
+        .testTarget(
+            name: "SSDBTests",
+            dependencies: [ "SSDB" ]
+        ),
+    ]
+)
