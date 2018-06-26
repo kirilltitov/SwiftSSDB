b5bf39978f2189f2be1c7573b2f09a779f233ea8
Kirill Titov <work@kirilltitov.com>
2018-06-26 23:11:48 +0300

    Removed legacy package description

--- Package.swift
+++ Package.swift
@@ -1,10 +1,24 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SSDB",
+    products: [
+        .library(name: "SSDB", targets: [ "SSDB" ]),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 0)
+        .package(url: "https://github.com/IBM-Swift/BlueSocket.git", .upToNextMajor(from: "1.0.0"))
+    ],
+    targets: [
+        .target(
+            name: "SSDB",
+            dependencies: [ "Socket" ]
+        ),
+        .testTarget(
+            name: "SSDBTests",
+            dependencies: [ "SSDB" ]
+        ),
     ]
 )
